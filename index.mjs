6847b550bd57c012ab8c6eacdaa006238e343271
hassana123 <hassanaabdll1@gmail.com>
2022-08-12 17:55:55 +0100

    final commit

--- index.mjs
+++ index.mjs
@@ -1,7 +1,9 @@
+
 let instuctions = document.querySelector(".popup-container");
 let startButtun = document.querySelector("#start-btn");
 let customContainer = document.querySelector(".container-custom");
 
+// adding functionalities to the instructions container
 startButtun.addEventListener("click", () =>{
     instuctions.style.display = "none";
     customContainer.style.display = "flex";
