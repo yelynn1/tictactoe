01691dd6c7ddc92eda2b57e1ee8ec68631f0596f
SwapnilRaj1708 <raj.swapnil1708@gmail.com>
2022-08-23 16:35:47 +0530

    "Play Game" button starts the game

--- index.mjs
+++ index.mjs
@@ -1,10 +1,15 @@
 
 let instuctions = document.querySelector(".popup-container");
-let startButtun = document.querySelector("#start-btn");
+let startButton = document.querySelector("#start-btn");
+let startButton2 = document.querySelector("#start-btn-2");
 let customContainer = document.querySelector(".container-custom");
 
 // adding functionalities to the instructions container
-startButtun.addEventListener("click", () =>{
+startButton.addEventListener("click", () =>{
+    instuctions.style.display = "none";
+    customContainer.style.display = "flex";
+})
+startButton2.addEventListener("click", () =>{
     instuctions.style.display = "none";
     customContainer.style.display = "flex";
 })
