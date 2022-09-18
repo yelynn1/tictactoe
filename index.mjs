4ea159831752aee0dc380486f37a824a124d0d67
Atharva Rajurkar <asrajurk@cougarnet.uh.edu>
2022-09-18 12:15:13 -0500

    Fixed Play Game button not working

--- index.mjs
+++ index.mjs
@@ -1,10 +1,20 @@
 
 let instuctions = document.querySelector(".popup-container");
 let startButtun = document.querySelector("#start-btn");
+let playGameButton = document.querySelector("#play-game-btn");
 let customContainer = document.querySelector(".container-custom");
 
 // adding functionalities to the instructions container
 startButtun.addEventListener("click", () =>{
+    startGame();
+})
+
+// adding functionalities to the instructions container
+playGameButton.addEventListener("click", () =>{
+    startGame();
+})
+
+function startGame(){
     instuctions.style.display = "none";
     customContainer.style.display = "flex";
-})
+}
