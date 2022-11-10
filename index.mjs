a55e26495270aa3ffc7f9f546951e7635beb4920
AnkanHalder <halderankan95@gmail.com>
2022-11-10 20:24:12 +0530

    Add two player support

--- index.mjs
+++ index.mjs
@@ -1,10 +1,17 @@
 
 let instuctions = document.querySelector(".popup-container");
-let startButtun = document.querySelector("#start-btn");
+let Player1Buttun = document.querySelector(".player1-btn");
+let Player2Buttun = document.querySelector(".player2-btn");
 let customContainer = document.querySelector(".container-custom");
 
 // adding functionalities to the instructions container
-startButtun.addEventListener("click", () =>{
+Player1Buttun.addEventListener("click", () =>{
+    singlePlayer();
+    instuctions.style.display = "none";
+    customContainer.style.display = "flex";
+})
+Player2Buttun.addEventListener("click", () =>{
+    twoPlayer();
     instuctions.style.display = "none";
     customContainer.style.display = "flex";
 })
